61933affd2dcdfff840a6529333cb755c61a03b0
sanjayy-j <sanjayjayakumar2006@gmail.com>
2026-06-09 15:19:20 +0530

    feat: add custom user model and item ownership

--- exchange/models.py
+++ exchange/models.py
@@ -1,9 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    pass
+
 
 class Item(models.Model):
+
+    class Status(models.TextChoices):
+        AVAILABLE = "AVAILABLE", "Available"
+        RESERVED = "RESERVED", "Reserved"
+        TRADED = "TRADED", "Traded"
+
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
     name = models.CharField(max_length=100)
 
     description = models.TextField()
+
+    status = models.CharField(
+        max_length=20,
+        choices=Status.choices,
+        default=Status.AVAILABLE
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
 
     def __str__(self):
         return self.name
