36184f609dbc084512737ac84302da25706eb3ef
sanjayy-j <sanjayjayakumar2006@gmail.com>
2026-06-15 15:53:44 +0530

    feat: implement item update and deletion with ownership permissions

--- exchange/permissions.py
+++ exchange/permissions.py
@@ -3,3 +3,7 @@ from rest_framework.permissions import BasePermission
 class IsAdminRole(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.role == request.user.Role.ADMIN
+    
+class IsOwnerOrAdmin(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_authenticated and (request.user.role == request.user.Role.ADMIN or obj.owner == request.user)
--- exchange/views.py
+++ exchange/views.py
@@ -8,7 +8,7 @@ from rest_framework.decorators import (
     permission_classes,
 )
 
-from .permissions import IsAdminRole
+from .permissions import IsAdminRole, IsOwnerOrAdmin
 from .models import Item
 from .serializers import ItemSerializer
 
@@ -72,3 +72,74 @@ class ItemDetailView(APIView):
             serializer.data,
             status=status.HTTP_200_OK
         ) 
+    
+    def patch(self, request, id):
+        try:
+            item = Item.objects.get(id=id)
+
+        except Item.DoesNotExist:
+            return Response(
+                {"error": "Item not found"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        permission = IsOwnerOrAdmin()
+
+        if not permission.has_object_permission(
+            request,
+            self,
+            item
+        ):
+            return Response(
+                {"error": "Forbidden"},
+                status=status.HTTP_403_FORBIDDEN
+            )
+
+        serializer = ItemSerializer(
+            item,
+            data=request.data,
+            partial=True
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK
+            )
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    
+    def delete(self, request, id):
+        try:
+            item = Item.objects.get(id=id)
+
+        except Item.DoesNotExist:
+            return Response(
+                {"error": "Item not found"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        permission = IsOwnerOrAdmin()
+
+        if not permission.has_object_permission(
+            request,
+            self,
+            item
+        ):
+            return Response(
+                {"error": "Forbidden"},
+                status=status.HTTP_403_FORBIDDEN
+            )
+
+        item.delete()
+
+        return Response(
+            {"message": "Item deleted successfully"},
+            status=status.HTTP_204_NO_CONTENT
+        )
+    
